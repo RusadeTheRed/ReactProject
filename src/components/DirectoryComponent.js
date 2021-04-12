aa7411f00c4fd33e72d03f960d3cd9d0d85ee079
Randell Anderson <zugzug07@gmail.com>
2021-04-12 15:28:57 -0500

    Presentational and Container Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,25 +1,13 @@
 import React, { Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-
+    
     render() {// props since we changed the location of the state to its own file 
         const directory = this.props.campsites.map(campsite => {
             return (// this works because we are in the array method "the more you know"
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -34,7 +22,6 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <CampsiteInfo campsite={this.state.selectedCampsite}/>
             </div>
         );
     }
